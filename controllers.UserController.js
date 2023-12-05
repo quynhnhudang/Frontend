9127bca1cb2906f7ddd623779ce1a33dded58d3c
Quynh Nhu Dang <87718231+quynhnhudang@users.noreply.github.com>
2023-12-05 17:46:15 -0500

    Create controllers.UserController.js

---
+++ controllers.UserController.js
@@ -0,0 +1,25 @@
+// UserController.js
+const User = require('../models/User');
+
+exports.register = async (req, res) => {
+  // Implement user registration logic
+};
+
+exports.login = async (req, res) => {
+  // Implement user login logic
+};
+
+// ContentController.js
+const Content = require('../models/Content');
+
+exports.getVocabulary = async (req, res) => {
+  // Implement logic to fetch vocabulary content
+};
+
+exports.getPronunciation = async (req, res) => {
+  // Implement logic to fetch pronunciation content
+};
+
+exports.getQuiz = async (req, res) => {
+  // Implement logic to fetch quiz content
+};
